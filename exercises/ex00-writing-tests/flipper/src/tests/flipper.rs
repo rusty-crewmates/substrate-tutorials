d173da1cf4f4660253a8e690f29d8a329ffefd38
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-06-06 11:44:37 -0400

    fix: make ex00 test fail

--- exercises/ex00-writing-tests/flipper/src/tests/flipper.rs
+++ exercises/ex00-writing-tests/flipper/src/tests/flipper.rs
@@ -4,23 +4,17 @@ use frame_support::{assert_noop, assert_ok};
 
 #[test]
 fn set_value_ok() {
-	new_test_ext().execute_with(|| {
-		// TODO: ensure the good behaviour of set_value() function.
-	});
+	new_test_ext().execute_with(|| todo!("ensure the good behaviour of set_value() function."));
 }
 
 #[test]
 fn set_value_err_already_set() {
-	new_test_ext().execute_with(|| {
-		// TODO: verify if the function returns the expected error.
-	});
+	new_test_ext().execute_with(|| todo!("verify if the function returns the expected error."));
 }
 
 #[test]
 fn flip_value_ok() {
-	new_test_ext().execute_with(|| {
-		// TODO: ensure the good behaviour of flip_value() function.
-	});
+	new_test_ext().execute_with(|| todo!("ensure the good behaviour of flip_value() function."));
 }
-// TODO: Make another test to check the behaviour in the case where an error occured in the
-// flip_function().
+
+todo!("Make another test to check the behaviour in the case where an error occured in the flip_function().");
